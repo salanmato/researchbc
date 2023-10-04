3def8050ba60c4c4a2256e42183d7de95eece89c
Alan Matos <60579487+salanmato@users.noreply.github.com>
2023-10-04 18:13:21 -0300

    enviando interesses
    agora, os valores de interesse são enviados ao banco de dados

--- server/index.js
+++ server/index.js
@@ -23,8 +23,15 @@ app.post('/api/create', (req, res) => {
     const nome = req.body.name;
     const ra = req.body.ra;
     const email = req.body.email;
+    const web = req.body.disciplinas.web;
+    const mobile = req.body.disciplinas.mobile;
+    const desktop = req.body.disciplinas.desktop;
+    const games = req.body.disciplinas.games;
+    const infra = req.body.disciplinas.infra;
+    const seguranca = req.body.disciplinas.seguranca;
+    const projetos = req.body.disciplinas.projetos;
 
-    db.query("INSERT INTO research_form (nome, ra, email) VALUES (?, ?, ?)",[nome, ra, email], (err, result) => {
+    db.query("INSERT INTO research_form (nome, ra, email, web, mobile, desktop, games, infra, seg, projetos) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",[nome, ra, email, web, mobile, desktop, games, infra, seguranca, projetos], (err, result) => {
         if (err) {
             console.log(err)
         }
