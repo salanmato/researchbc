97d026e57ac282c3e939e58766e1d463cf9ec4c5
Alan Matos <60579487+salanmato@users.noreply.github.com>
2023-10-04 21:07:14 -0300

    Update showList.js

--- javascript/showList.js
+++ javascript/showList.js
@@ -1,43 +1,7 @@
 const respostas = document.querySelector('.respostas')
 
-<<<<<<< Updated upstream
-
-<<<<<<< Updated upstream
-//mostrando as respostas
-function answerList(){
-    let answers = JSON.parse(localStorage.getItem('respostas'))
-    let table = document.querySelector('.respostas')
-    
-    
-    answers.forEach(answer => {
-        //linha
-        let row = document.createElement('tr')
-        //itens
-        let nome = document.createElement('td')
-        let ra = document.createElement('td')
-        let email = document.createElement('td')
-        let web = document.createElement('td')
-        let mobile = document.createElement('td')
-        let desktop = document.createElement('td')
-        let games = document.createElement('td')
-        let infra = document.createElement('td')
-        let seguranca = document.createElement('td')
-        let projetos = document.createElement('td')
-
-        //inserindo informação
-        nome.innerText = answer.name
-        ra.innerText = answer.ra
-        email.innerText = answer.email
-        web.innerText = answer.disciplinas.web
-        mobile.innerText = answer.disciplinas.mobile
-        desktop.innerText = answer.disciplinas.desktop
-        games.innerText = answer.disciplinas.games
-        infra.innerText = answer.disciplinas.infra
-        seguranca.innerText = answer.disciplinas.seguranca
-        projetos.innerText = answer.disciplinas.projetos
-=======
-=======
 const url = 'http://localhost:3002/api/get'
+let lista = []
 fetch(url).then(response => response.json())
     .then(data => data.map(answer => {
         //linha
@@ -54,22 +18,17 @@ fetch(url).then(response => response.json())
         let seguranca = document.createElement('td')
         let projetos = document.createElement('td')
 
->>>>>>> Stashed changes
         //inserindo informação
         nome.innerText = answer.nome
         ra.innerText = answer.ra
         email.innerText = answer.email
-        web.innerText = answer.web
-        mobile.innerText = answer.mobile
-        desktop.innerText = answer.desktop
-        games.innerText = answer.games
-        infra.innerText = answer.infra
-        seguranca.innerText = answer.seg
-        projetos.innerText = answer.projetos
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+        web.innerText = answer.disciplinas.web
+        mobile.innerText = answer.disciplinas.mobile
+        desktop.innerText = answer.disciplinas.desktop
+        games.innerText = answer.disciplinas.games
+        infra.innerText = answer.disciplinas.infra
+        seguranca.innerText = answer.disciplinas.seguranca
+        projetos.innerText = answer.disciplinas.projetos
 
         row.appendChild(nome)
         row.appendChild(ra)
@@ -82,21 +41,6 @@ fetch(url).then(response => response.json())
         row.appendChild(seguranca)
         row.appendChild(projetos)
 
-<<<<<<< Updated upstream
         respostas.appendChild(row)
-<<<<<<< Updated upstream
-    })
-
-    
-}
-
-answerList()
-=======
-        respostas.appendChild(row)}))
-    .catch(error => console.log(error))
-    
->>>>>>> Stashed changes
-=======
     }))
     .catch(error => console.log(error))
->>>>>>> Stashed changes
